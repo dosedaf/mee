36a5a8491bbd426d3c6eb1122ff4aaea6f916c8e
dosedaf <mash1o1o1o1@gmail.com>
2026-08-24 21:31:18 +0700

    bruh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,3 @@ export default defineConfig({
   site: 'https://dosedaf.github.io',
   base: '/mee',
 });
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
